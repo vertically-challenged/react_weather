4b4b0224cff7c5dd9b8528c5c8eab0dc92f046b3
vertically-challenged <50969768+vertically-challenged@users.noreply.github.com>
2022-05-02 16:14:08 +0300

    Disabled eslint in some files

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
+/* eslint-disable */
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const linkElement = screen.getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
+})
